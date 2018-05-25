8f1101d1869cdb1195ae3ade18c1e58492fa4543
sam <smarks@origamisoftware.com>
2018-05-24 20:23:56 -0400

    show how the build breaks

---
+++ src/main/java/edu/studentname/advancedjava/App.java
@@ -0,0 +1,13 @@
+package edu.studentname.advancedjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
